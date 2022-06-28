276873d5224ed9f5d959d4febd730a7bede8daa1
legen92 <confessional111@gmail.com>
2022-06-28 12:20:14 +0700

    A

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const authRouter = require("./routers/auth.router");
 const bookRouter = require("./routers/book.router");
+const cors = require('cors')
 require("dotenv").config()
 
 const app = express();
 const port = process.env.PORT || 3333
-
+app.use(cors({origin: ['https://store-backed.herokuapp.com/book']}))
 app.use(bodyParser.json());
 
 app.use("/auth", authRouter);
